f9a3624948909facbd70da119c4c33bc022e7c3f
dinar6 <didem.yanardag@bil.omu.edu.tr>
2015-03-30 02:51:58 +0300

    düzenlendi

--- php/form.php
+++ php/form.php
@@ -7,14 +7,14 @@
 <form action = "kontrol.php" method = "post" >
 <h1 align="center">Kayit Formu</h1>
 <center>
-<table cellpadding = "7" cellspacing = "7" bgcolor = #7F7F7F >
-<tr><td>Ad-Soyad:</td><td><input type = "text" name = "adSoyad"/><td></tr>
-<tr><td>Yaş:</td><td><input type = "text" name = "yas"/><td></tr>
-<tr><td>Şehir:</td><td><select name = "sehir"/>
-<option value = "eskisehir">Eskişehir</option>
-<option value = "istanbul">İstanbul</option>
-<option value = "ankara">Ankara</option></select><td></tr>
-<tr><td></td><td><input type = "submit" value = "Gönder"/></td></tr>
-</table></center>
+        <table cellpadding = "7" cellspacing = "7" bgcolor = #7F7F7F >
+	<tr><td>Ad-Soyad:</td><td><input type = "text" name = "adSoyad"/><td></tr>
+	<tr><td>Yaş:</td><td><input type = "text" name = "yas"/><td></tr>
+	<tr><td>Şehir:</td><td><select name = "sehir"/>
+	<option value = "eskisehir">Eskişehir</option>
+	<option value = "istanbul">İstanbul</option>
+	<option value = "ankara">Ankara</option></select><td></tr>
+	<tr><td></td><td><input type = "submit" value = "Gönder"/></td></tr>
+	</table></center>
 </form>
 </body></html>
